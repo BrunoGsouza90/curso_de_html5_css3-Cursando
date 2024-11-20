7db0e0b7432221f2e6b76ad8b5a14b87b66b9aa5
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2024-11-19 23:37:20 -0300

    Aula 111

--- Módulo 4/Aulas/Aula 108 - Menu responsivo/script.js
+++ Módulo 4/Aulas/Aula 108 - Menu responsivo/script.js
@@ -3,12 +3,24 @@ let menu = document.getElementsByTagName('menu')[0]
 
 burguer.addEventListener('click', ()=>{
 
-    setTimeout(()=>{
-        if(menu.style.display == 'none'){
-            menu.style.display = 'block'
-        } else {
-            menu.style.display = 'none'
-        }
-    }, 0)
+    if(menu.style.display == 'none'){
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+
+})
+
+window.addEventListener('resize', ()=>{
+
+    if(window.innerWidth >= 768){
+
+        menu.style.display = 'block'
+
+    } else {
+
+        menu.style.display = 'none'
+
+    }
 
 })
